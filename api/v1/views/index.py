30c24380a72f3f5ae3bc15b24884122cc646bb89
nicolas <nicolas.quoirez@hotmail.fr>
2024-04-17 15:30:30 +0200

    modif index.py: add route "/stats"

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,13 +2,35 @@
 """ This module defines the routes for API.
 
 Routes:
-    /status: display JSON status --> 'OK'.
+    /status: Return JSON status --> 'OK'.
+    /stats: Return a list of the sum of all objects by type.
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.user import User
+from models.review import Review
+from models.state import State
 
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
-    """return JSON status: OK."""
+    """ Endpoint that return JSON status: OK."""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route("/stats")
+def stats():
+    """ Endpoint that return a list of the number of each objects by type """
+    objs = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "users": storage.count(User),
+        "reviews": storage.count(Review),
+        "states": storage.count(State)
+    }
+    return jsonify(objs)

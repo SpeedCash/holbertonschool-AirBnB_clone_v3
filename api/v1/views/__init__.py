63a0a4e37cdfd4d7ea3e79e836045bc3d237d4f7
nicolas <nicolas.quoirez@hotmail.fr>
2024-04-19 11:48:15 +0200

    task 7

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,4 +7,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # The nexts lines raise an error pycodestyle, it's normal.
 from api.v1.views.index import *
-from api.v1.views.states import *
+from api.v1.views import states, cities, amenities, users, places, places_reviews
